3b1fdbe69c9c85d3c91173d9d510a03e08f552c3
pocket0589 <fealty@snu.ac.kr>
2016-03-06 17:04:27 +0900

    message

--- crawling/naver/views.py
+++ crawling/naver/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def remove_html_tags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('\n',data)
+
+def get_single_article(item_url):
+    source_code = requests.get(item_url)
+    plain_text=source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    title = soup.select('#articleTitle')
+    url=item_url
+    publisher=soup.select('.press_logo > a > img')[0].attrs['title']
+    print(publisher)
+        
+    #title, url, author, publisher, date
+    print(type(title))
+    for contents in soup.select('#articleBodyContents'):
+        print(contents.prettify())
+
+get_single_article('http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=052&aid=0000791309')
