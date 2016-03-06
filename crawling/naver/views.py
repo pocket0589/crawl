ccdc46399da7585c0625ccbf66cb651fbbac32bd
dlkorea <bright14u@gmail.com>
2016-03-06 20:18:34 +0900

    크롤링 후 장고 통해 db 저장하는 이그잼플(차후 모델 재수정 필요)

--- crawling/naver/views.py
+++ crawling/naver/views.py
@@ -12,14 +12,24 @@ def get_single_article(item_url):
     source_code = requests.get(item_url)
     plain_text=source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
-    title = soup.select('#articleTitle')
+    title = soup.select('#articleTitle')[0].get_text()
     url=item_url
     publisher=soup.select('.press_logo > a > img')[0].attrs['title']
-    print(publisher)
-        
+
     #title, url, author, publisher, date
-    print(type(title))
-    for contents in soup.select('#articleBodyContents'):
-        print(contents.prettify())
+    for content in soup.select('#articleBodyContents'):
+        try:
+            content_list.append(content)
+        except NameError:
+            content_list = [content]
+
+    article_data = {
+        'title': title,
+        'url': url,
+        'publisher': publisher,
+        'content_list': content_list,
+    }
+
+    return article_data
 
 get_single_article('http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=052&aid=0000791309')
